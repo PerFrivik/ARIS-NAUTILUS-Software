d3c69a8696e2842ebc22c7649b730719d1de22f4
PerFrivik <94360401+PerFrivik@users.noreply.github.com>
2022-04-05 11:55:50 +0200

    Update_code_structure
    Main idea presented, no way-points yet.

--- Code/UUV_Functions.py
+++ Code/UUV_Functions.py
@@ -1,11 +1,18 @@
+#Author(s): Per Frivik (pfrivik@student.ethz.ch)
+#Date last change: 05.04.2022 
+
 from doctest import master
 import time
-# Import mavutil
 from pymavlink import mavutil
+import sys
+import math
+from pymavlink.quaternion import QuaternionBase
+
 
 ####################################################
 # Function import test                             #
 ####################################################
+
 def test_function():
     print("Hello NAUTILUS!")
 
@@ -13,6 +20,7 @@ def test_function():
 ####################################################
 # Establishing a connection to the UUV             #
 ####################################################
+
 def create_connection(): 
     # Create the connection
     #  If using a companion computer
@@ -29,12 +37,77 @@ def create_connection():
     # Make sure the connection is valid
     master.wait_heartbeat()
 
+    return(master)
+
+
+####################################################
+# Depth the UUV                                    #
+####################################################
+
+def set_target_depth(depth, boot):
+    boot_time = boot
+        #  Sets the target depth while in depth-hold mode.
+
+        # Uses https://mavlink.io/en/messages/common.html#SET_POSITION_TARGET_GLOBAL_INT
+
+        # 'depth' is technically an altitude, so set as negative meters below the surface
+        #     -> set_target_depth(-1.5) # sets target to 1.5m below the water surface.
+
+        
+    master.mav.set_position_target_global_int_send(
+            int(1e3 * (time.time() - boot_time)), # ms since boot
+            master.target_system, master.target_component,
+            coordinate_frame=mavutil.mavlink.MAV_FRAME_GLOBAL_INT,
+            type_mask=( # ignore everything except z position
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_X_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_Y_IGNORE |
+                # DON'T mavutil.mavlink.POSITION_TARGET_TYPEMASK_Z_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_VX_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_VY_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_VZ_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_AX_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_AY_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_AZ_IGNORE |
+                # DON'T mavutil.mavlink.POSITION_TARGET_TYPEMASK_FORCE_SET |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_YAW_IGNORE |
+                mavutil.mavlink.POSITION_TARGET_TYPEMASK_YAW_RATE_IGNORE
+            ), lat_int=0, lon_int=0, alt=depth, # (x, y WGS84 frame pos - not used), z [m]
+            vx=0, vy=0, vz=0, # velocities in NED frame [m/s] (not used)
+            afx=0, afy=0, afz=0, yaw=0, yaw_rate=0
+            # accelerations in NED frame [N], yaw, yaw_rate
+            #  (all not supported yet, ignored in GCS Mavlink)
+        )
+
+####################################################
+# Setting the attitude of the UUV                  #
+####################################################
+
+def set_target_attitude(roll, pitch, yaw, boot):
+    boot_time = boot
+
+        #  Sets the target attitude while in depth-hold mode.
+
+        # 'roll', 'pitch', and 'yaw' are angles in degrees.
+
+    master.mav.set_attitude_target_send(
+            int(1e3 * (time.time() - boot_time)), # ms since boot
+            master.target_system, master.target_component,
+            # allow throttle to be controlled by depth_hold mode
+            mavutil.mavlink.ATTITUDE_TARGET_TYPEMASK_THROTTLE_IGNORE,
+            # -> attitude quaternion (w, x, y, z | zero-rotation is 1, 0, 0, 0)
+            QuaternionBase([math.radians(angle) for angle in (roll, pitch, yaw)]),
+            0, 0, 0, 0 # roll rate, pitch rate, yaw rate, thrust
+        )
+
+
 ####################################################
 # Arming the UUV                                   #
 ####################################################
-def ARM():
-    # https://mavlink.io/en/messages/common.html#MAV_CMD_COMPONENT_ARM_DISARM #
 
+def ARM(master_connection):
+    master = master_connection
+
+    # https://mavlink.io/en/messages/common.html#MAV_CMD_COMPONENT_ARM_DISARM #
     # Arm
     # master.arducopter_arm() or:
 
@@ -58,7 +131,8 @@ def ARM():
 # Disarming the UUV                                #
 ####################################################
 
-def DISARM():
+def DISARM(master_connection):
+    master = master_connection
 
     # Disarm
     # master.arducopter_disarm() or:
@@ -70,15 +144,127 @@ def DISARM():
         0, 
         
         0, 0, 0, 0, 0, 0)
+    
+    # wait until disarming confirmed
+    print("Waitng for the vehicle to disarm")
+    master.motors_disarmed_wait()
+    print('Disarmed!')
 
+
+####################################################
+# Choosing Modes                                   #
+####################################################
+
+def Mode_selection(requested_mode, master_connection):
+    master = master_connection
+
+    # Choose a mode
+    mode = requested_mode 
+
+    # Check if mode is available
+    if mode not in master.mode_mapping():
+        print('Unknown mode : {}'.format(mode))
+        print('Try:', list(master.mode_mapping().keys()))
+        sys.exit(1)
+
+    # Get mode ID
+    mode_id = master.mode_mapping()[mode]
+    # Set new mode
+    # master.mav.command_long_send(
+    #    master.target_system, master.target_component,
+    #    mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
+    #    0, mode_id, 0, 0, 0, 0, 0) or:
+    # master.set_mode(mode_id) or:
+    master.mav.set_mode_send(
+        master.target_system,
+        mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, mode_id)
+
+    # Not sure if we need this code!!!!!!
+    # while not master.wait_heartbeat().custom_mode == DEPTH_HOLD_MODE:
+    #     master.set_mode(DEPTH_HOLD)
+
+    #The following code only checks if the sent command is acknowledged by the UUV or not
+
+    while True:
+        # Wait for ACK command
+        ack_msg = master.recv_match(type='COMMAND_ACK', blocking=True)
+        ack_msg = ack_msg.to_dict()
+
+        # Check if command in the same in `set_mode`
+        if ack_msg['command'] != mavutil.mavlink.MAVLINK_MSG_ID_SET_MODE:
+            continue
+
+        # [Delete] if you see this, it means for real testing delete the code below and use the else
+        # else:
+        #     break
+
+        # Print the ACK result !
+        print(mavutil.mavlink.enums['MAV_RESULT'][ack_msg['result']].description)
+        break
+
+####################################################
+# Get all vehicle paremeters (only for testing!)   #
+####################################################
+
+def Parameters(master_connection):
+    master = master_connection
+
+    # Request all parameters, Parameter list: https://www.ardusub.com/developers/full-parameter-list.html
+    master.mav.param_request_list_send(
+        master.target_system, master.target_component
+    )
+    while True:
+        time.sleep(0.01)
+        try:
+            message = master.recv_match(type='PARAM_VALUE', blocking=True).to_dict()
+            print('name: %s\tvalue: %d' % (message['param_id'],
+                                        message['param_value']))
+        except Exception as error:
+            print(error)
+            sys.exit(0)
+
+####################################################
+# Starting_procedure                               #
+####################################################
+
+def Starting_procedure(master_connection):
+    master = master_connection
+
+    boot_time = time.time()
+
+    #Define wanted depth for start
+
+    depth = #value
+
+    set_target_depth(depth, boot_time)
+
+    #Define wanted roll, pitch and yaw value for start
+      
+    roll = #value
+    pitch = #value
+    yaw = #value
+    set_target_attitude(roll, pitch, yaw, boot_time)
+
+
+    # arm ArduSub autopilot and wait until confirmed
+    ARM()
+
+    # set the desired operating mode
+    Mode_selection('ALT_HOLD', master)
+
+  
 
 ####################################################
 # Get some information !                           #
 ####################################################
-def data():
+
+def data(master_connection):
+    master = master_connection
+
     while True:
         try:
             print(master.recv_match().to_dict())
         except:
             pass
         time.sleep(0.1)
+
--- Code/UUV_Topside.py
+++ Code/UUV_Topside.py
@@ -1,20 +1,25 @@
-from UUV_Functions import test_function, create_connection, ARM, DISARM, Mode_selection, data
+from Code.UUV_Functions import Starting_procedure
+from UUV_Functions import test_function, create_connection, Mode_selection, data, Starting_procedure
 from doctest import master
 import time
 from pymavlink import mavutil
 import sys
+import math
+from pymavlink.quaternion import QuaternionBase
+
 
 #Test function import from UUV_Functions.py
 test_function()
 
 #Establishing a connection to the UUV
-create_connection()
+master = create_connection()
 
-#Request Mode 0 MAV_MODE_PREFLIGHT	System is not ready to fly, booting, calibrating, etc. No flag is set.
-Mode_selection('MAV_MODE_PREFLIGHT')
+Starting_procedure(master)
 
-#Request Mode 216 MAV_MODE_GUIDED_ARMED	System is allowed to be active, under autonomous control, manual setpoint
-Mode_selection('MAV_MODE_GUIDED_ARMED')
+
+
+
+
 
 
 
