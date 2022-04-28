0228b4b04c64d175c78f64c16a629ee99e18214e
PerFrivik <94360401+PerFrivik@users.noreply.github.com>
2022-04-28 13:31:51 +0200

    update

--- Code/NEWUUV/UUV_New_Functions.py
+++ Code/NEWUUV/UUV_New_Functions.py
@@ -9,36 +9,34 @@ master = mavutil.mavlink_connection('udpin:localhost:14540')
 
 # Wait for the first heartbeat 
 #   This sets the system and component ID of remote system for the link
-the_connection.wait_heartbeat()
+master.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
 
-# Once connected, use 'the_connection' to get and send messages
-
-####################################################
-# Getting data from the UUV                        #
-####################################################
-
-def data(master):
-  
-    master.mav.statustext_send(mavutil.mavlink.name)
+# Once connected, use 'master' to get and send messages
 
 ####################################################
 # Getting Pressure data                            #
 ####################################################
 
-def pressure_data(master)
+def Pressure_data(master):
 
     msg_pressure = master.recv_match(type='MAV_SYS_STATUS_SENSOR_ABSOLUTE_PRESSURE', blocking = True)
-    return msg_pressure
+    return msg_Pressure_filter
 
 ####################################################
-# Getting GPS data                            #
+# Getting GPS data                                 #
 ####################################################
+
+def GPS_data(master):
+
+    msg_GPS = master.recv_match(type='MAV_SYS_STATUS_SENSOR_ABSOLUTE_PRESSURE', blocking = True)
+    return msg_GPS_filter
 
 ####################################################
 # Getting Compass data                            #
 ####################################################
 
-####################################################
-# Getting data                            #
-####################################################
+def Compass_data(master):
+
+    msg_Compass = master.recv_match(type='MAV_SYS_STATUS_SENSOR_ABSOLUTE_PRESSURE', blocking = True)
+    return msg_Compass_filter 
